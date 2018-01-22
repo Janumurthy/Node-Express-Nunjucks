6f79677b41e6aeec95b24531143bce2d1851b4fb
janumurthy <ljanani92@gmail.com>
2018-01-22 17:37:42 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const express= require('express')
+const app=express()
+const nunjucks = require('nunjucks')
+var njIncludeData = require('nunjucks-includeData');
+
+var path = require('path');
+var nunjucksEnv = nunjucks.configure('html',{
+	autoescape:true,
+	express:app	
+});
+
+njIncludeData.install(nunjucksEnv); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req,res){
+	res.render(path.join(__dirname,'html','/index.html')	)
+});
+
+var server = app.listen(8082, function () {
+   var host = 'localhost'
+   var port = server.address().port
+   console.log("Listening at http://%s:%s", host, port)
+})
